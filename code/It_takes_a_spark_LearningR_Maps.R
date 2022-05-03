0e61ff5c658c70eadca008a3aa04618ad5a9a56e
shaziaruybal <shaziaruybal@gmail.com>
2022-05-03 16:51:13 +1000

    update map script with github repo urls

--- code/It_takes_a_spark_LearningR_Maps.R
+++ code/It_takes_a_spark_LearningR_Maps.R
@@ -1,18 +1,15 @@
-
 #################### School map #################
 dev.off()
 # Load the map file
-map <- load(file="~/WEHI/R_ladies/It_Takes_Spark/Map.rda")
+map <- load(url("https://raw.githubusercontent.com/shaziaruybal/2022-05-04-It-takes-a-spark/master/data/Map.rda"))
+
 # Load the coordinates data of the students
-map_data=read.delim("~/WEHI/R_ladies/It_Takes_Spark/Coordinates_student_data.txt")
+map_data = read.delim("https://raw.githubusercontent.com/shaziaruybal/2022-05-04-It-takes-a-spark/master/data/Coordinates_student_data.txt")
 
 # Plot the map
 plot(melb_map)
 
 # Add points with color for the sick students
 points(map_data$Longitude[map_data$Sickness == "Y"], map_data$Latitude[map_data$Sickness == "Y"], col = "red", cex = 5)
-# Add points with color for the healthyn students
+# Add points with color for the healthy students
 points(map_data$Longitude[map_data$Sickness == "N"], map_data$Latitude[map_data$Sickness == "N"], cex = 5)
-
-
-
